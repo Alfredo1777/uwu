cba6382c9132e2f738f939c67674d2136ce264d3
Alumno <Alumno@PC51>
2019-11-07 11:33:03 -0600

    olamaster

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <iostream>
+using namespace std;
+//Prototipo de funcion
+void intercambio(int*, int*);
+
+int main()
+{
+	int a, b;
+	cout << "Introduce el primer valor: ";
+	cin >> a;
+	cout << "Introduce el segundo valor: ";
+	cin >> b;
+	cout << endl;
+	cout << "Valor de a: " << a << endl<<
+		"valor de b: " << b << endl;
+	intercambio(&a, &b);
+	cout << endl << "Despues del intercambio: " <<
+		endl << endl;
+	cout << "Valor de la a: " << a << "Valor de la b: "
+		<< b << endl;
+	system("pause");
+	return 0;
+}
+void intercambio(int* x, int* y)
+{
+	int z;
+	z = *x;
+	*x = *y;
+	*y = z;
+}
